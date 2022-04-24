2aaa88e7341a0070a5298d26521792a0f6e543c3
Mateusz Kopij <sagasu@gmail.com>
2022-04-24 12:10:05 +0100

    playground

--- numpy-playground/playground.py
+++ numpy-playground/playground.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 ar = np.arange(4)
+ar_from_list = np.array([1, 2, 3, 4], dtype=np.int8) # by deafult dtype is int32, dtype is optional
+
+print(type(ar_from_list[0]))
 
 print(ar)
 print(type(ar))
