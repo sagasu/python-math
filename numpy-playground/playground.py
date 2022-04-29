6f78b25bf3badca2756c180b8248a0e1c36989eb
Mateusz Kopij <sagasu@gmail.com>
2022-04-29 21:03:59 +0100

    zeros, ones

--- numpy-playground/playground.py
+++ numpy-playground/playground.py
@@ -5,5 +5,11 @@ ar_from_list = np.array([1, 2, 3, 4], dtype=np.int8) # by deafult dtype is int32
 
 print(type(ar_from_list[0]))
 
+empty_array  = np.zeros((2, 3))
+print(empty_array)
+
+array_ones = np.ones((2, 3))
+print(array_ones)
+
 print(ar)
 print(type(ar))
